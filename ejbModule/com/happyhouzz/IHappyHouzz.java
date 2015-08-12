14c2c61b2cb7e61130933482113122022174ec00
gunagara <gunagara@HYDDTL911823.corp.capgemini.com>
2015-08-12 14:40:56 +0530

    Initial Commit

---
+++ ejbModule/com/happyhouzz/IHappyHouzz.java
@@ -0,0 +1,15 @@
+package com.happyhouzz;
+
+/**
+ * Session Bean implementation class IHappyHouzz
+ */
+public class IHappyHouzz {
+
+    /**
+     * Default constructor. 
+     */
+    public IHappyHouzz() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
